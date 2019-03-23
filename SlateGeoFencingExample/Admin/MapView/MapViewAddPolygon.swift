704c6128ce8de540448448c259cd5ce20eb95ee7
BrandonWong <brandonwong@BrandonWongs-MacBook-Pro.local>
2019-03-23 22:42:12 +0800

    Add Admin Polygon

---
+++ SlateGeoFencingExample/Admin/MapView/MapViewAddPolygon.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewAddPolygon.swift
+//  SlateGeoFencingExample
+//
+//  Created by BrandonWong on 23/03/2019.
+//  Copyright © 2019 Silverlake Mobility Ecosystem Sdn Bhd. All rights reserved.
+//
+
+import UIKit
+
+class MapViewAddPolygon: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
