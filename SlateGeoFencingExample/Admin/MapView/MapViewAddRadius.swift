b937008d1b00df12b68269dc7d60f1c39340df08
BrandonWong <brandonwong@BrandonWongs-MacBook-Pro.local>
2019-03-23 17:04:36 +0800

    Done Admin Add Radius

---
+++ SlateGeoFencingExample/Admin/MapView/MapViewAddRadius.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewAddRadius.swift
+//  SlateGeoFencingExample
+//
+//  Created by BrandonWong on 23/03/2019.
+//  Copyright © 2019 Silverlake Mobility Ecosystem Sdn Bhd. All rights reserved.
+//
+
+import UIKit
+
+class MapViewAddRadius: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
