5b374b7f84cf6dc8700769f3a058c9d1ef83db9f
BrandonWong <brandonwong@BrandonWongs-MacBook-Pro.local>
2019-03-24 03:13:51 +0800

    Consumer module on-going

---
+++ SlateGeoFencingExample/Consumer/MapView/ConsumerMapViewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConsumerMapViewViewController.swift
+//  SlateGeoFencingExample
+//
+//  Created by BrandonWong on 24/03/2019.
+//  Copyright © 2019 Silverlake Mobility Ecosystem Sdn Bhd. All rights reserved.
+//
+
+import UIKit
+
+class ConsumerMapViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
